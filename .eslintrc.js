60779d71516e3b3cea1b697b36468ec156fe67fd
Kevin Lai <knkevinlai@gmail.com>
2020-05-06 16:16:11 +0800

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  // parser: 'babel-eslint',
+  extends: [
+    // ESSENTIALS
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    // OPTIONAL
+  ],
+  plugins: [
+    'emotion',
+    'jest',
+  ],
+  rules: {
+    // ENABLE THIS SINCE: next.js does not require importing React at the top of the components everytime.
+    // "react/react-in-jsx-scope": "off",
+    "emotion/jsx-import": "off",
+    "emotion/no-vanilla": "error",
+    "emotion/import-from-emotion": "error",
+    "emotion/styled-import": "error",
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    'jest/globals': true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2017,
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+}
